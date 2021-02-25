104d52d578b765f01012a2ef75c4afcc7616b485
fbedn <f_ramos@outlook.com>
2021-02-25 09:27:34 -0300

    Adicionando início do projeto Spring

---
+++ games-extra-api/src/main/java/com/gamesextra/fbedn/gamesextraapi/GamesExtraApiApplication.java
@@ -0,0 +1,13 @@
+package com.gamesextra.fbedn.gamesextraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesExtraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesExtraApiApplication.class, args);
+	}
+
+}
---
+++ games-extra-api/src/test/java/com/gamesextra/fbedn/gamesextraapi/GamesExtraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesextra.fbedn.gamesextraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesExtraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
